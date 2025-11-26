8534a5771ce4e8fbb81288494df43c3561747d78
hemanth.gowda <hemanth.gowda@ominaya.com>
2025-11-26 20:52:37 +0530

    update strucutre

--- app/api/get-nse-option-chain-live/service.ts
+++ app/api/get-nse-option-chain-live/service.ts
@@ -2,6 +2,8 @@ import { getTotOIRatio } from "@/app/lib/services/calculations/calculations";
 import { getOptionChainIndices } from "@/app/lib/services/nseindia";
 import { NSEOptionChainResponse } from "@/app/lib/types/nseindia/nseindiaType";
 import { APIResponseType } from "@/app/lib/types/response/serviceResponseType";
+import { NSEOCTotalOIRatio } from "@/lib/generated/prisma/client";
+import prisma from "@/lib/prisma";
 
 async function getOptionChainIndicesService(): Promise<APIResponseType<any>> {
   // get data from getOptionChainIndices
@@ -11,12 +13,25 @@ async function getOptionChainIndicesService(): Promise<APIResponseType<any>> {
   if (response.code !== "S001" || !response.data) {
     return response;
   }
+
+  const NSEOptionChainData: NSEOptionChainResponse = response.data;
   // get totratio
   const totRatio = getTotOIRatio(response.data);
 
-  console.log("TOT OI RATIO:", totRatio.totOIRatio);
-
   // save to database
+
+  const resp = await prisma.nSEOCTotalOIRatio.create({
+    data: {
+      ce_total_oi: NSEOptionChainData.filtered.CE.totOI,
+      pe_total_oi: NSEOptionChainData.filtered.PE.totOI,
+      last_fetched_date: NSEOptionChainData.records.timestamp,
+      ratio: totRatio.totOIRatio,
+      created_date: new Date(),
+      last_updated_date: new Date(),
+    },
+  });
+
+  console.log("Saved NSEOCTotalOIRatio:", resp);
 
   return {
     code: "S001",
--- app/lib/services/calculations/calculations.ts
+++ app/lib/services/calculations/calculations.ts
@@ -1,11 +1,11 @@
 import { NSEOptionChainResponse } from "../../types/nseindia/nseindiaType";
 
-function getTotOIRatio(data: NSEOptionChainResponse): { totOIRatio: string } {
+function getTotOIRatio(data: NSEOptionChainResponse): { totOIRatio: number } {
   // get
   const totOICE = data.filtered.CE.totOI;
   const totOIPE = data.filtered.PE.totOI;
 
-  const totOIRatio = (totOICE / totOIPE).toFixed(2);
+  const totOIRatio = parseFloat((totOICE / totOIPE).toFixed(2));
 
   return { totOIRatio };
 }
